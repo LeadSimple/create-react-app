236c5e5a647969ec10b81a1b5b02cf5bd667ba10
Christopher Berkompas <chris@leadsimple.net>
2019-01-09 09:14:52 -0800

    Port changes from separate webpack configs to the new single config file used by CRA

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -70,7 +70,8 @@ module.exports = function(webpackEnv) {
   // NOTE: any changes here need to be mirrored in start.js
   const DEFAULT_PORT = parseInt(process.env.PORT, 10) || 3000;
   const HOST = process.env.HOST || '0.0.0.0';
-  var devServer = HOST + ':' + DEFAULT_PORT;
+  const protocol = process.env.HTTPS === 'true' ? 'https' : 'http';
+  var devServer = protocol + '://' + HOST + ':' + DEFAULT_PORT;
 
   // Webpack uses `publicPath` to determine where the app is being served from.
   // It requires a trailing slash, or the file assets will get an incorrect path.
@@ -159,27 +160,28 @@ module.exports = function(webpackEnv) {
       : isEnvDevelopment && 'cheap-module-source-map',
     // These are the "entry points" to our application.
     // This means they will be the "root" imports that are included in JS bundle.
-    entry: [
-      // Include an alternative client for WebpackDevServer. A client's job is to
-      // connect to WebpackDevServer by a socket and get notified about changes.
-      // When you save a file, the client will either apply hot updates (in case
-      // of CSS changes), or refresh the page (in case of JS changes). When you
-      // make a syntax error, this client will display a syntax error overlay.
-      // Note: instead of the default WebpackDevServer client, we use a custom one
-      // to bring better experience for Create React App users. You can replace
-      // the line below with these two lines if you prefer the stock client:
-      // require.resolve('webpack-dev-server/client') + '?/',
-      // require.resolve('webpack/hot/dev-server'),
-      isEnvDevelopment &&
-        // require.resolve('react-dev-utils/webpackHotDevClient'),
-        require.resolve('webpack-dev-server/client') + '?' + devServer + '/sockjs-node',
-        require.resolve('webpack/hot/dev-server'),
-      // Finally, this is your app's code:
-      paths.appIndexJs,
-      // We include the app code last so that if there is a runtime error during
-      // initialization, it doesn't blow up the WebpackDevServer client, and
-      // changing JS code would still trigger a refresh.
-    ].filter(Boolean),
+    entry: {
+      // Load multiple entry points, from ../src/entries
+      ...paths.appEntryPoints.reduce((result, cur) => {
+        const entry_name = path.basename(cur).replace(/\.[^/.]+$/, "") // trim file extensions off the end
+        result[entry_name] = [
+          // Include an alternative client for WebpackDevServer. A client's job is to
+          // connect to WebpackDevServer by a socket and get notified about changes.
+          // When you save a file, the client will either apply hot updates (in case
+          // of CSS changes), or refresh the page (in case of JS changes). When you
+          // make a syntax error, this client will display a syntax error overlay.
+          // Note: instead of the default WebpackDevServer client, we use a custom one
+          // to bring better experience for Create React App users. You can replace
+          // the line below with these two lines if you prefer the stock client:
+          // isEnvDevelopment && require.resolve('react-dev-utils/webpackHotDevClient'),
+          isEnvDevelopment && require.resolve('webpack-dev-server/client') + '?' + devServer + '/sockjs-node',
+          isEnvDevelopment && require.resolve('webpack/hot/dev-server'),
+          // Finally, this is your app's code:
+          cur,
+        ].filter(Boolean); // Remove undefined lines
+        return result;
+      }, {}),
+    },
     output: {
       // The build folder.
       path: isEnvProduction ? paths.appBuild : undefined,
